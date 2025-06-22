7b9573dd5c9128e68fb03d17472c8f0cd393c076
BlueBloxKraft <94107880+bluebloxkraft@users.noreply.github.com>
2025-06-22 09:05:11 +0530

    Update snake.py
    easier customisability + other stuff

--- python/snake.py
+++ python/snake.py
@@ -12,38 +12,36 @@ class MovementDirection(Enum):
         RIGHT = (1, 0)
         LEFT = (-1, 0)
 
-def get_opposite(mvdir):
-    if mvdir == MovementDirection.UP:
-        return MovementDirection.DOWN
-    elif mvdir == MovementDirection.DOWN:
-        return MovementDirection.UP
-    elif mvdir == MovementDirection.RIGHT:
-        return MovementDirection.LEFT
-    elif mvdir == MovementDirection.LEFT:
-        return MovementDirection.RIGHT
+
+block_size = 30                         # size of each block in px
+world_size = (15, 15)                   # the size of the plane (width, height) in terms of block_size
+snake_spawn = (7, 7)                    # spawnpoint of the snake in the plane
+default_mvdir = MovementDirection.DOWN  # the direction in which the snake moves in when the game starts
+gametick_spacing = 5                    # after how many frames should the game udate
+fps = 30                                # frame rate
 
 
 pg.init()
-screen = pg.display.set_mode((450, 450))
+screen = pg.display.set_mode((world_size[0] * block_size, world_size[1] * block_size))
 pg.display.set_caption("Snake")
 clock = pg.time.Clock()
 running = True
 
-snake_body = [(7, 7)]
+snake_body = [snake_spawn]
 apple = 0
-current_direction = MovementDirection.DOWN
-queue_direction = MovementDirection.DOWN
+current_direction = default_mvdir
+queue_direction = default_mvdir
 
 gametick_spacing_count = 0
 
 def draw():
     for s in snake_body:
-        pg.draw.rect(screen, (0, 255, 0), (s[0] * 30, s[1] * 30, 30, 30))
+        pg.draw.rect(screen, (0, 255, 0), (s[0] * block_size, s[1] * block_size, block_size, block_size))
 
-    pg.draw.rect(screen, (40, 100, 40), (snake_body[0][0] * 30, snake_body[0][1] * 30, 30, 30))
+    pg.draw.rect(screen, (40, 100, 40), (snake_body[0][0] * block_size, snake_body[0][1] * block_size, block_size, block_size))
 
     if apple != 0:
-        pg.draw.rect(screen, (255, 0, 0), (apple[0] * 30, apple[1] * 30, 30, 30))
+        pg.draw.rect(screen, (255, 0, 0), (apple[0] * block_size, apple[1] * block_size, block_size, block_size))
 
 def gameover():
     global running
@@ -57,15 +55,15 @@ def game_tick():
     global apple
     
     if apple == 0:
-        new_apple = (random.randint(0, 14), random.randint(0, 14))
+        new_apple = (random.randint(0, world_size[0] - 1), random.randint(0, world_size[1] - 1))
         while new_apple in snake_body:
-            new_apple = (random.randint(0, 14), random.randint(0, 14))
+            new_apple = (random.randint(0, world_size[0] - 1), random.randint(0, world_size[1] - 1))
         apple = new_apple
 
     current_direction = queue_direction
     next_block = tuple(a + b for a, b in zip(snake_body[0], current_direction.value))
 
-    if not (0 <= next_block[0] < 15) or not (0 <= next_block[1] < 15):
+    if not (0 <= next_block[0] < world_size[0] and 0 <= next_block[1] < world_size[1]):
         gameover()
 
     if next_block in snake_body:
@@ -73,7 +71,7 @@ def game_tick():
     
     snake_body.insert(0, next_block)
 
-    if len(snake_body) == 15*15:
+    if len(snake_body) == world_size[0] * world_size[1]:
         gameover()
         print("YOU FINISHED THE GAME!!!!\n(if i got the condition right lol)")
     
@@ -84,17 +82,17 @@ def game_tick():
 
 def update_direction(mvdir):
     global queue_direction
-    if len(snake_body) == 1 or mvdir != get_opposite(current_direction):
+    if len(snake_body) == 1 or mvdir.value != tuple(-x for x in current_direction.value):
         queue_direction = mvdir
 
 def keyhandling(key):
-        if key == pg.K_UP:
+        if key == pg.K_w:
             update_direction(MovementDirection.UP)
-        elif key == pg.K_DOWN:
+        elif key == pg.K_s:
             update_direction(MovementDirection.DOWN)
-        elif key == pg.K_RIGHT:
+        elif key == pg.K_d:
             update_direction(MovementDirection.RIGHT)
-        elif key == pg.K_LEFT:
+        elif key == pg.K_a:
             update_direction(MovementDirection.LEFT)
         
 
@@ -112,12 +110,12 @@ while running:
     
     pg.display.flip()
 
-    if gametick_spacing_count > 2:
+    if gametick_spacing_count > gametick_spacing:
         gametick_spacing_count = 0
         game_tick()
     else:
         gametick_spacing_count += 1
     
-    clock.tick(30)
+    clock.tick(fps)
 
 pg.quit()
