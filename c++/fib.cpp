18f6df93c81d65c9133f4082e06af5b76564567e
bbk <bluebloxkraftzx08@gmail.com>
2025-10-01 19:36:35 +0530

    Made the code better (in terms of functionality). Now works as it should!

--- c++/fib.cpp
+++ c++/fib.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 
-using namespace std;
+int fib(int n){
+   if(n == 0)
+      return 0;
 
-int fib(int n) {
-	int n0 = 1;
-	int  n1 = 0;
-	int n2 = 1;
-	
-	for(int k = 1; k < n; k++){
-		n1 = n2;
-		n2 = n0;
-		n0 = n1 + n2;
-	}
-	
-	return n0;	
+   int F1 = 0;
+   int F2 = 1;
+   int F3 = 1;
 
+   for(int i = 1; i < n; i++){
+      F3 = F2 + F1;
+      F1 = F2;
+      F2 = F3;
+   }
+
+   return F3;
 }
 
-int main() {
-        int n;
-	cout << "Enter the index of the number in the fibonacci sequence you want to find: ";
-	cin >> n;
-	cout << to_string(fib(n)) << endl;
-	return 0;
+int main(){
+   std::cout << "Enter a number: ";
+   int x;
+   std::cin >> x;
+   std::cout << "The Fibonacci number at index " << x << " is " << fib(x) << "\n";
 }
