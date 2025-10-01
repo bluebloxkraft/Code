a9f915bf477be14ae40d590b1211cb6f27e997a7
bbk <bluebloxkraftzx08@gmail.com>
2025-10-01 19:29:13 +0530

    A thinf that outputs the von Neumann representation of a natural number

---
+++ python/vonNeumann_representation.py
@@ -0,0 +1,16 @@
+known = {0: "∅"}
+
+def von_neumann(n: int):
+    global known
+    if n in known:
+        return known[n]
+    else:
+        str = "{" + von_neumann(0)
+        for i in range(1, n):
+            str += f", {von_neumann(i)}"
+        str += "}"
+        known[n] = str
+        return str;
+
+# test (omg it grows so fast, damn)
+print(von_neumann(15))
