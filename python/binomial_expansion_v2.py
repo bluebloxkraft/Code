5b0dcf73904ca797ea3fbb95de747d98f23dff8f
bbk <bluebloxkraftzx08@gmail.com>
2025-10-12 18:49:14 +0530

    uses the recurrece relation of the binomial coffient instead of the factorial definition 	new file:   binomial_expansion_v2.py

---
+++ python/binomial_expansion_v2.py
@@ -0,0 +1,40 @@
+print("Binomial expansion! Yay!")
+x = int(input("Enter a number: "))
+
+C_known: {(int, int): int} = {0: 0}
+
+def C(n: int, k: int) -> int:
+    if k == 0 or n == k:
+        return 1
+    if k == 1:
+        return n
+    if (n, k) not in C_known:
+        C_known[(n, k)] = C(n - 1, k - 1) + C(n - 1, k)
+    return C_known[(n, k)]
+
+def superscript_num(n: int) -> str:
+        if n == 1:
+            return ""
+        return str(n).replace("0", "⁰").replace("1", "¹").replace("2", "²").replace("3", "³").replace("4", "⁴").replace("5", "⁵").replace("6", "⁶").replace("7", "⁷").replace("8", "⁸").replace("9", "⁹")
+
+def get_term_formatted(c: int, a: int, b: int) -> str:
+    res = ""
+
+    if c != 1:
+        res = str(c)
+    if a != 0:
+        res += f"x{superscript_num(a)}"
+    if b != 0: 
+        res += f"y{superscript_num(b)}"
+    
+    return res    
+
+def main():
+   terms: [str] = []
+   
+   for i in range(x + 1):
+      terms.append(get_term_formatted(C(x, i), x - i, i))
+   
+   print("The expansion is:", " + ".join(terms))
+
+main()
